8433c6ec23f7972e146657d1e9bc5caba9d99a53
Facon <cppapprentice@gmail.com>
2014-04-03 14:47:48 +0200

    Ejemplo con JavaMail completado :D .

--- SeguridadOTP/src/es/deusto/pruebamail/Mail.java
+++ SeguridadOTP/src/es/deusto/pruebamail/Mail.java
@@ -1,12 +1,54 @@
 package es.deusto.pruebamail;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class Mail {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final String username = "alumnodeustoseguridad@gmail.com";
+		final String password = "seguridaddelainformacion";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("alumnodeustoseguridad@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("cppapprentice@gmail.com"));
+			message.setSubject("Contraseña OTP");
+			message.setText("Esta es la contraseña que tienes que copiar,"
+					+ "\n\n 1455669");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 
 	}
 
